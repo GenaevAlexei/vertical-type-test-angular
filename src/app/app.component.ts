842414b38b106528a8fd6f28beb20905bcb6ea1b
Alexei <genaev282@mail.ru>
2020-10-23 15:18:06 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  animations:[
+    trigger('fadedLetter', [
+      transition('void => *',[
+        style ({opacity: 0}),
+        animate(3000, style ({opacity: 1}))
+      ])
+    ])
+  ]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'vertical-type-test-angular';
-}
+
+  lettersArray: any[] = [];
+
+  constructor(){}
+
+  ngOnInit() {}
+
+  keyHandler(event: KeyboardEvent){
+     let keyValue = event.key;
+     let regexp = /[a-zA-Zа-яА-ЯёЁ]/i;
+     if (regexp.test(keyValue) === true && keyValue.length === 1) {
+
+        this.lettersArray.push(keyValue);
+        console.log(this.lettersArray);
+     }
+   }
+ }
